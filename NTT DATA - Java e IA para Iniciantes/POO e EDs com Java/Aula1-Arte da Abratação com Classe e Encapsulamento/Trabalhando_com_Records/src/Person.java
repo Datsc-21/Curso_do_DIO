a951265d73737a2ff3e65dbb04c2705402b1af86
RockDev21 <danny.scholze@unemat.br>
2025-07-16 14:03:14 -0400

    Record

---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula1-Arte da Abratação com Classe e Encapsulamento/Trabalhando_com_Records/src/Person.java
@@ -0,0 +1,18 @@
+import javax.annotation.processing.SupportedSourceVersion;
+
+public record Person(String name, int age ) {
+
+public Person{
+
+}
+
+    public Person(String name) {
+        this(name, 1);
+    }
+
+    public  String getInfo(){
+    return "Name: " + name + " age: " + age;
+}
+
+
+}
