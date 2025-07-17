59b56ecf0ce8be5763fe11594a5a74e168d03957
RockDev21 <danny.scholze@unemat.br>
2025-07-17 15:09:05 -0400

    Novas implementações

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula2-Herença e Polimorfismo/Aula1-HP/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula2-Herença e Polimorfismo/Aula1-HP/src/Main.java
@@ -2,16 +2,46 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-      // Employee employee = new Employee();
-       Employee  manager = new Manager();
+    printEmployee(new Manager());
+        printEmployee(new Salesman());
 
-       manager.setName("Maria");
-     //  manager.setLogin("Maria");
-      // manager.setPassword("1234");
+  }
+
+    //Método
+    public static void printEmployee(Employee employee) {
+        System.out.printf("=======%s===========\n", employee.getClass().getCanonicalName());
+
+        switch (employee){
+            case Manager manager ->{
+                manager.setCode("123");
+                manager.setName("Maria");
+                manager.setSalary(5000);
+                manager.setLogin("Maria");
+                manager.setPassword("1234");
+                manager.getCommission(1200);
+
+                System.out.println(manager.getCode());
+                System.out.println(manager.getSalary());
+                System.out.println(manager.getLogin());
+                System.out.println(manager.getPassword());
+                System.out.println(manager.getName());
+                System.out.println(manager.getCommission());
+            }
+
+            case Salesman salesman -> {
+               salesman.setCode("456");
+                salesman.setName("Maria");
+                salesman.setSalary(2800);
+                salesman.setPercentPerSold(10);
 
 
-       System.out.println(manager.getName());
-       // System.out.println(manager.getLogin());
-       // System.out.println(manager.getPassword());
+                System.out.println(salesman.getCode());
+                System.out.println(salesman.getSalary());
+                System.out.println(salesman.getName());
+                System.out.println(salesman.getPercentPerSold());
+            }
+        }
+        System.out.println("====================");
     }
+
 }
