c11ebcdbc758b3b9f2e31205480b3bd040344ed8
RockDev21 <danny.scholze@unemat.br>
2025-07-17 12:35:41 -0400

    Introduçao a Herança e Polimorfismo

---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula2-Herença e Polimorfismo/Aula1-HP/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       Employee employee = new Employee();
+       Manager manager = new Manager();
+
+       manager.setName("Maria");
+       manager.setLogin("Maria");
+       manager.setPassword("1234");
+
+
+       System.out.println(manager.getName());
+        System.out.println(manager.getLogin());
+        System.out.println(manager.getPassword());
+    }
+}
---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula2-Herença e Polimorfismo/Aula1-HP/src/Salesman.java
@@ -0,0 +1,12 @@
+public class Salesman extends Employee{ //Extensão-subclasse
+
+    private double percentPerSold;
+
+    public double getPercentPerSold() {
+        return percentPerSold;
+    }
+
+    public void setPercentPerSold(double percentPerSold) {
+        this.percentPerSold = percentPerSold;
+    }
+}
