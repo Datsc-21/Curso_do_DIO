0594e142ef375413f13d1d586dda3c9a404e21d9
Danny Scholze <dannyscholze21@gmail.com>
2025-07-20 15:11:14 -0400

    Simplificando as interfaces funcionais

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula3-Dominando Interfaces e Lambda/InterfaceFuncional/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula3-Dominando Interfaces e Lambda/InterfaceFuncional/src/Main.java
@@ -1,12 +1,21 @@
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         List<User> users = List.of(new User("Maria", 17), new User("João", 20), new User("Danny", 21));
-    users.forEach(System.out::println);
+
+    printStringValue(Record::toString, users);
+    
 
     }
+
+    private static void printStringValue(Function<User, String> callback, List<User> users){
+       users.forEach( u ->  System.out.println(callback.apply(u)));
+    }
+
 }
+
