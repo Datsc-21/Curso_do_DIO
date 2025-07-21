916580a4c1aeac782141b61e5742274974d457e2
RockDev21 <danny.scholze@unemat.br>
2025-07-21 14:28:49 -0400

    Class String

---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part3_1-ClasseString/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+     var value = "java;java;java;java";
+     value = value.replaceFirst("j", "J");
+     System.out.println(value);
+
+    }
+}
