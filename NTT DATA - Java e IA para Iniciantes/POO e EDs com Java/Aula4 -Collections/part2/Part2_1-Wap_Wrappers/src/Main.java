6b1b930316ecdbe735141107aecfeb1d9e85569a
Danny Scholze <dannyscholze21@gmail.com>
2025-07-20 20:39:42 -0400

    Atualizando

---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part2/Part2_1-Wap_Wrappers/src/Main.java
@@ -0,0 +1,17 @@
+import domain.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, User> users = new HashMap<>();
+        users.put("danny@danny.com", new User("danny", 21));
+        users.put("maria@,maria.com", new User("maria", 17));
+        users.put("luis@luis.com", new User("luis", 19));
+        users.put("mateus@mateus.com", new User("mateus", 18));
+
+        System.out.println(users);
+
+    }
+}
