53107a2cf120b9c7ab8f94a1b007fa6a06c90e69
RockDev21 <danny.scholze@unemat.br>
2025-07-21 15:23:29 -0400

    Recomeçando

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part3_1-ClasseString/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part3_1-ClasseString/src/Main.java
@@ -4,20 +4,11 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
      var value = """
-             {"name":"Danny","age":21}
-             """;
+             {"name":"Danny","age":21}""";
         Map<String, String> map = new HashMap<>();
-        while(map.size() != 2){
-            var starKeyIndex = value.indexOf("\"");
-            var endKeyIndex = value.indexOf("\":");
-            var keyValue = value.substring(starKeyIndex + 1, endKeyIndex);
+       value = value.replace("{", "").replace("}", "").replace("\"", "");
+       var valueArr = value.split(",");
 
-            var startValueIndex = value.indexOf(":\"");
-            var endValueIndex = value.indexOf("\":", startValueIndex);
-            var valueKey = value.substring(starKeyIndex + 1, endValueIndex);
-            map.put(keyValue, valueKey);
-
-        }
 System.out.println(map);
 
     }
