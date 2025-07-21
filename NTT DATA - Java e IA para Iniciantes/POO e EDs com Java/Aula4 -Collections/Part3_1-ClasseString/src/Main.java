7e018e878994385fc344933ad43694f2ab506046
RockDev21 <danny.scholze@unemat.br>
2025-07-21 14:53:08 -0400

    toUpperCase e toLowerCase

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part3_1-ClasseString/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part3_1-ClasseString/src/Main.java
@@ -3,9 +3,8 @@ public class Main {
     public static void main(String[] args) {
      var value = "java;java;java;java";
 
-     var values = value.split(";", 2);
-     for(var v : values){
-         System.out.println(v);
-     }
+   System.out.println(value.toUpperCase());
+        System.out.println(value.toLowerCase());
+
     }
 }
