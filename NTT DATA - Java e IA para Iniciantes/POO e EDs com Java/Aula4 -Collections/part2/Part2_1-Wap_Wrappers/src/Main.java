525b7b4f22895ce67b49b5d5b4cabef8fb9771d6
Danny Scholze <dannyscholze21@gmail.com>
2025-07-20 21:02:19 -0400

    Algo não está certo

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part2/Part2_1-Wap_Wrappers/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part2/Part2_1-Wap_Wrappers/src/Main.java
@@ -11,7 +11,11 @@ public class Main {
         users.put("luis@luis.com", new User("luis", 19));
         users.put("mateus@mateus.com", new User("mateus", 18));
 
-        System.out.println(users.remove("deka@deka.com", new User("maria", 17)));
+        users.forEach((k, v) -> System.out.printf("key: %s | value %s \n", k, v));
+        System.out.println("==========================================");
+        users.replace("deka@deka.com", new User("danny", 29));
+        users.forEach((k, v) -> System.out.printf("key: %s | value %s \n", k, v));
+
 
 
     }
