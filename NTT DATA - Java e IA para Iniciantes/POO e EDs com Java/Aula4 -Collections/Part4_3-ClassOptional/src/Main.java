35727c58506d237a77dbdf45df95998bc5850869
RockDev21 <danny.scholze@unemat.br>
2025-07-23 17:31:28 -0400

    Criando um código com Optional

---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_3-ClassOptional/src/Main.java
@@ -0,0 +1,16 @@
+import domain.User;
+
+import java.util.Optional;
+
+import static domain.SexEnum.FAMALE;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Optional<User> optional = Optional.of(new User("Danny", 21, FAMALE));
+        System.out.println(optional.isPresent());
+
+
+
+    }
+}
---
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_3-ClassOptional/src/domain/SexEnum.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum SexEnum {
+
+    MALE, FAMALE
+}
