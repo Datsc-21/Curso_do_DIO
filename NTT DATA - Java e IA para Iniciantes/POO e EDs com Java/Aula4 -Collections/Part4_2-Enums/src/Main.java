2f0af43d5c9f37dbd78a8141e960110f8b6db848
RockDev21 <danny.scholze@unemat.br>
2025-07-23 16:59:06 -0400

    Adicionando verificações

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_2-Enums/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_2-Enums/src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+var scanner = new Scanner(System.in);
 
         var option = -1;
         while (option != 5){
@@ -11,10 +12,14 @@ public class Main {
             System.out.println("3 - Multiplicação");
             System.out.println("4 - Divisão");
             System.out.println("5 - Sair");
+            option = scanner.nextInt();
 
-            
+            if(option > 5 || option < 1){
+                System.out.println("Selecione uma opção válida");
+                continue;
+            }
 
-
+           if(option == 5) break;
         }
 
     }
