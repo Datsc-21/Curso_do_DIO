bc206b38fd2636d00eb6605d4895e7e37873a52b
RockDev21 <danny.scholze@unemat.br>
2025-07-23 16:20:33 -0400

    Testando com float, 2f

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_1-ClasseBigDecimal/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_1-ClasseBigDecimal/src/Main.java
@@ -3,21 +3,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        var value1 = 0.1;
-        var value2 = 0.2;
+        var value1 = 0.1f;
+        var value2 = 0.2f;
         System.out.println(value1 + value2);
         System.out.println("=======================");
 
-        System.out.println(2.00 - 1.1);
-        System.out.println(2.00 - 1.2);
-        System.out.println(2.00 - 1.3);
-        System.out.println(2.00 - 1.4);
-        System.out.println(2.00 - 1.5);
-        System.out.println(2.00 - 1.6);
-        System.out.println(2.00 - 1.7);
-        System.out.println(2.00 - 1.8);
-        System.out.println(2.00 - 1.9);
-        System.out.println(2.00 - 2);
+        System.out.println(2.00f - 1.1f);
+        System.out.println(2.00f - 1.2f);
+        System.out.println(2.00f - 1.3f);
+        System.out.println(2.00f - 1.4f);
+        System.out.println(2.00f - 1.5f);
+        System.out.println(2.00f - 1.6f);
+        System.out.println(2.00f - 1.7f);
+        System.out.println(2.00f - 1.8f);
+        System.out.println(2.00f - 1.9f);
+        System.out.println(2.00f - 2f);
 
     }
 }
