c73ee8c4dfc04e503584fe9c5b43df27dcbe559f
RockDev21 <danny.scholze@unemat.br>
2025-07-23 16:36:47 -0400

    mutiply

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_1-ClasseBigDecimal/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/Part4_1-ClasseBigDecimal/src/Main.java
@@ -5,21 +5,9 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-     var value1 = new BigDecimal("0.1");
-     var value2 = new BigDecimal("0.2");
-     System.out.println(value1.add(value2));
-     System.out.println("=========================");
-
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.1")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.2")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.3")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.4")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.5")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.6")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.7")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.8")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.9")));
-     System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("2")));
+     var value1 = new BigDecimal("56.32434");
+     var value2 = new BigDecimal("99.242");
+     System.out.println(value2.multiply(value1));
 
 
 
