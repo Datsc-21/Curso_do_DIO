cabe18a91ceaf4c91ce714761f8f35900f5744b6
RockDev21 <danny.scholze@unemat.br>
2025-07-30 16:13:20 -0400

    trabalhando distinct

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -7,7 +8,7 @@ public class Main {
 
 
         var value = Stream.of(1,2,3,4,5,6,7).
-               reduce(0, Integer::sum );
+               distinct().collect(Collectors.toList());
 
         System.out.println(value);
     }
