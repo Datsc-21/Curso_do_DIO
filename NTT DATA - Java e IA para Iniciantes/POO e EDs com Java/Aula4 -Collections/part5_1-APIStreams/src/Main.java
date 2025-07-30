57f772adc6319197f19a595c6f550075f8e277f3
RockDev21 <danny.scholze@unemat.br>
2025-07-30 16:22:54 -0400

    Finalizando apistream

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
@@ -11,8 +11,9 @@ List<Integer> value2 = List.of(1,2,3,4,5,6,7,8,9,10,11,12);
 
 var newValues = value2.stream()
         .filter(value1::contains)
-        .peek(System.out::println)
+        .peek(n -> System.out.printf("Filter %s \n", n))
         .map(n -> value1.stream().reduce(n, (n1,n2) -> n1 -n2))
+        .peek(n -> System.out.printf("Map %s \n", n))
         .collect(Collectors.toSet());
 
         System.out.println(newValues);
