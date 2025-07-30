df46c15f16a4d9e14a96136517460bb41e7b834f
RockDev21 <danny.scholze@unemat.br>
2025-07-30 15:14:38 -0400

    testando

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
@@ -5,13 +5,12 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> debugValues = new ArrayList<>();
+
         var value = Stream.of("Maria", "João", "Marcio", "Luana", "Leandro", "Marcia").
-                peek(debugValues::add).
-                filter(name -> name.endsWith("a"))
+                filter(name -> name.endsWith("o"))
+                .limit(2)
                  .toList();
 
-        System.out.println(debugValues);
         System.out.println(value);
     }
 }
