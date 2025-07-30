ad70765ac90ca35cd65cb77da96284d83b245e9f
RockDev21 <danny.scholze@unemat.br>
2025-07-30 17:21:33 -0400

    Criando os métodos

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_3-Generics/src/dao/GenericDAO.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_3-Generics/src/dao/GenericDAO.java
@@ -1,4 +1,29 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class GenericDAO<T> {
+
+    private List<T> db = new ArrayList<>();
+
+    public T save(T domain){
+        db.add(domain);
+        return domain;
+    }
+
+    public T update(T domain){
+       var stored = db.stream().filter(d -> d.equals(domain)).
+               findFirst().orElseThrow();
+
+       db.remove(stored);
+       db.add(domain);
+       return save(domain);
+    }
+
+    public boolean delete(T domain){
+        return db.remove(domain);
+    }
+
+
 }
