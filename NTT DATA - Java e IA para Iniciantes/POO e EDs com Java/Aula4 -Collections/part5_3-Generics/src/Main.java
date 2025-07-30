8638e4f6027a9eb69e4227431c550e467b378c64
RockDev21 <danny.scholze@unemat.br>
2025-07-30 17:43:22 -0400

    atualizando e testando

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_3-Generics/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_3-Generics/src/Main.java
@@ -6,17 +6,17 @@ import domain.UserDomain;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static GenericDAO<UserDomain> dao = new UserDAO();
+    private final static GenericDAO<UserDomain> dao = new UserDAO();
 
     public static void main(String[] args) {
 
         var user = new UserDomain("Danny", 21);
-  System.out.println(dao.count());
- System.out.println(dao.save(new UserDomain("Danny", 21)));
- System.out.println(dao.findAll());
         System.out.println(dao.count());
-        dao.delete(new UserDomain("", -1));
-dao.delete(user);
+ System.out.println(dao.save(user));
+ System.out.println(dao.findAll());
+ System.out.println(dao.count());
+ System.out.println(dao.delete(new UserDomain("", -1)));
+ System.out.println(dao.delete(user));
 System.out.println(dao.findAll());
 System.out.println(dao.count());
 
