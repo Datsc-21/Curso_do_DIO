f557c04d1c37ea47ab027addd45e1f34b1f0e508
RockDev21 <danny.scholze@unemat.br>
2025-07-30 16:47:20 -0400

    Adicinando os dados dos usuários

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_2-APIStreams/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_2-APIStreams/src/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static domain.ContactType.EMAIL;
+import static domain.Sex.FEMALE;
 import static domain.Sex.MALE;
 
 public class Main {
@@ -15,35 +16,35 @@ public class Main {
 
     private static List<User> generateUsers(){
         var contacts1 = List.of(
-                new Contact("", ContactType.PHONE),
-                new Contact("", EMAIL)
+                new Contact("(65)93234-4450", ContactType.PHONE),
+                new Contact("andreia@gmail.com", EMAIL)
         );
 
         var contacts2 = List.of(
-                new Contact("", ContactType.PHONE),
-                new Contact("", EMAIL)
+                new Contact("(65)92223-5545", ContactType.PHONE),
+                new Contact("danny@gmail.com", EMAIL)
         );
 
         var contacts3 = List.of(
-                new Contact("", ContactType.PHONE),
-                new Contact("", EMAIL)
+                new Contact("(65)98832-9978", ContactType.PHONE),
+                new Contact("pedro@gmail.com", EMAIL)
         );
 
         var contacts4 = List.of(
-                new Contact("", ContactType.PHONE),
-                new Contact("", EMAIL)
+                new Contact("(65)97722-3345", ContactType.PHONE),
+                new Contact("luis@gmail.com", EMAIL)
         );
 
         var contacts5 = List.of(
-                new Contact("", ContactType.PHONE),
-                new Contact("", EMAIL)
+                new Contact("(65)92223-1123", ContactType.PHONE),
+                new Contact("maria@gmail.com", EMAIL)
         );
 
-        var user1 = new User("", 0, MALE, new ArrayList<>(contacts1));
-        var user2 = new User("", 0, MALE, new ArrayList<>(contacts2));
-        var user3 = new User("", 0, MALE, new ArrayList<>(contacts3));
-        var user4 = new User("", 0, MALE, new ArrayList<>(contacts4));
-        var user5 = new User("", 0, MALE, new ArrayList<>(contacts5));
+        var user1 = new User("Andreia", 30, FEMALE, new ArrayList<>(contacts1));
+        var user2 = new User("Danny", 21, FEMALE, new ArrayList<>(contacts2));
+        var user3 = new User("Pedro", 19, MALE, new ArrayList<>(contacts3));
+        var user4 = new User("Luis", 19, MALE, new ArrayList<>(contacts4));
+        var user5 = new User("Maria", 17, FEMALE, new ArrayList<>(contacts5));
 
 
 
