60f5a1c3bc9d06a8678eba4b9d50ecba255af60f
RockDev21 <danny.scholze@unemat.br>
2025-07-30 15:10:32 -0400

    debugvalues com peek

--- NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/POO e EDs com Java/Aula4 -Collections/part5_1-APIStreams/src/Main.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<String> debugValues = new ArrayList<>();
         var value = Stream.of("Maria", "João", "Marcio", "Luana", "Leandro", "Marcia").
-                peek(System.out::println).
-                filter(name -> name.endsWith("a"));
+                peek(debugValues::add).
+                filter(name -> name.endsWith("a"))
+                 .toList();
 
-
+        System.out.println(debugValues);
         System.out.println(value);
     }
 }
