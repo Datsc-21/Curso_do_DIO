235d266a13e2be8e810442b733491efbca8c6658
Danny Scholze <dannyscholze21@gmail.com>
2025-08-02 20:52:18 -0400

    O método usermodel criado

--- NTT DATA - Java e IA para Iniciantes/Debugging em Java/Debugging e o Tratamento de Exceções em Java/Debugging e Tratamento de Exceções/Debugging_Excecoes/src/Main.java
+++ NTT DATA - Java e IA para Iniciantes/Debugging em Java/Debugging e o Tratamento de Exceções em Java/Debugging e Tratamento de Exceções/Debugging_Excecoes/src/Main.java
@@ -1,7 +1,52 @@
+import br.com.dio.dao.UserDAO;
+import br.com.dio.model.MenuOption;
+import br.com.dio.model.UserModel;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+
+    private final static UserDAO dao = new UserDAO();
+
+  private final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        System.out.println("Bem vindo ao cadastro de usuários, selecione a operação desejada");
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Atualizar");
+        System.out.println("3 - Excluir");
+        System.out.println("4 - Buscar por identificador");
+        System.out.println("5 - Listar");
+        System.out.println("6 - Sair");
+        var userInput= scanner.nextInt();
+        while(true){
+            var selectedOption = MenuOption.values()[userInput -1];
+            switch (selectedOption){
+                case SAVE -> {
+                    var user = requestUserInfo();
+                }
+            }
+
+
+        }
 
 
     }
+
+    private static UserModel requestUserInfo(){
+        System.out.println("Informe o nome do usuário: ");
+        var name = scanner.next();
+        System.out.println("Informe o e-mail do usuário: ");
+        var email = scanner.next();
+        System.out.println("Informe a data de nascimento do usuário (dd/mm/yyyy): ");
+        var birthdayString = scanner.next();
+        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var birthday = OffsetDateTime.parse(birthdayString, formatter);
+        return new UserModel(0, name, email, birthday);
+
+    }
+
 }
--- NTT DATA - Java e IA para Iniciantes/Debugging em Java/Debugging e o Tratamento de Exceções em Java/Debugging e Tratamento de Exceções/Debugging_Excecoes/src/br/com/dio/dao/UserDAO.java
+++ NTT DATA - Java e IA para Iniciantes/Debugging em Java/Debugging e o Tratamento de Exceções em Java/Debugging e Tratamento de Exceções/Debugging_Excecoes/src/br/com/dio/dao/UserDAO.java
@@ -37,4 +37,8 @@ public class UserDAO {
                 orElseThrow(() -> new UserNotFoundException(message));
     }
 
+    public List<UserModel> findAll(){
+        return models;
+    }
+
 }
