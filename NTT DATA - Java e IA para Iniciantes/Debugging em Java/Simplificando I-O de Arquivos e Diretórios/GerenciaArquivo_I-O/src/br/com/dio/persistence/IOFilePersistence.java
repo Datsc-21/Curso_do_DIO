1fd4717aa3efece18a3299a6a1b83de08a837b92
RockDev21 <danny.scholze@unemat.br>
2025-08-04 15:43:55 -0400

    Arquivo criado

--- NTT DATA - Java e IA para Iniciantes/Debugging em Java/Simplificando I-O de Arquivos e Diretórios/GerenciaArquivo_I-O/src/br/com/dio/persistence/IOFilePersistence.java
+++ NTT DATA - Java e IA para Iniciantes/Debugging em Java/Simplificando I-O de Arquivos e Diretórios/GerenciaArquivo_I-O/src/br/com/dio/persistence/IOFilePersistence.java
@@ -2,6 +2,9 @@ package br.com.dio.persistence;
 
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class IOFilePersistence implements  FilePersistence{
 
@@ -9,9 +12,12 @@ public class IOFilePersistence implements  FilePersistence{
     private final String storedDir = "/managedFiles/IO/";
     private final String filename;
 
-    public IOFilePersistence(String filename) {
+    public IOFilePersistence(String filename) throws IOException {
         this.filename = filename;
         var file = new File(currentDir + storedDir);
+        if(!file.exists() && !file.mkdirs()) throw new IOException("Erro ao criar arquivo");
+
+        clearFile();
     }
 
 
@@ -39,4 +45,15 @@ public class IOFilePersistence implements  FilePersistence{
     public String findBy(String sentence) {
         return "";
     }
+
+    private void clearFile(){
+        try(OutputStream outputStream = new FileOutputStream(currentDir + storedDir + filename) ) {
+           System.out.printf("inicializando recursos (%s) \n", currentDir + storedDir + filename);
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+
 }
