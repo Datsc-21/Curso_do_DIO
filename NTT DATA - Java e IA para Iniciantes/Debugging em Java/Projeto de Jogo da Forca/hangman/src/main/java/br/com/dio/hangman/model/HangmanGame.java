22c0eac4311215239fce97c0e996dad8682ca559
RockDev21 <danny.scholze@unemat.br>
2025-08-18 17:58:42 -0400

    Status definido

--- NTT DATA - Java e IA para Iniciantes/Debugging em Java/Projeto de Jogo da Forca/hangman/src/main/java/br/com/dio/hangman/model/HangmanGame.java
+++ NTT DATA - Java e IA para Iniciantes/Debugging em Java/Projeto de Jogo da Forca/hangman/src/main/java/br/com/dio/hangman/model/HangmanGame.java
@@ -2,22 +2,27 @@ package br.com.dio.hangman.model;
 
 import java.util.List;
 
+import static br.com.dio.hangman.model.HangmanGameStatus.PENDING;
+
 public class HangmanGame {
 
     private String hangman;
+    private HangmanGameStatus hangmanGameStatus;
 
     public HangmanGame(final List<HangmanChar> characters) {
         var whiteSpace = " ".repeat(characters.size());
         var characterSpace = "-".repeat(characters.size());
+        this.hangmanGameStatus = PENDING;
+        buildHangmanDesign(whiteSpace, characterSpace);
     }
 
     @Override
     public String toString() {
-        return "HangmanGame{}";
+        return this.hangman;
     }
 
     private void buildHangmanDesign(final String whiteSpaces, final String characterSpaces){
-         hangman = "  -----  " + whiteSpaces + System.lineSeparator() +
+         this.hangman = "  -----  " + whiteSpaces + System.lineSeparator() +
                    "  |   |  " + whiteSpaces + System.lineSeparator() +
                    "  |   |  " + whiteSpaces + System.lineSeparator() +
                    "  |      " + whiteSpaces + System.lineSeparator() +
@@ -25,9 +30,6 @@ public class HangmanGame {
                    "  |      " + whiteSpaces + System.lineSeparator() +
                    "  |      " + whiteSpaces + System.lineSeparator() +
                    "=========" + whiteSpaces + System.lineSeparator();
-
-
-
-
+         
     }
 }
