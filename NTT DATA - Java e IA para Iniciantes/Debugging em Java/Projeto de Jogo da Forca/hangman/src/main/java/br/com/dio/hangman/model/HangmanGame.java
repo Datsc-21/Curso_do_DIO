ee01f64282b22cab6b128307cba39a3e8affb495
Danny Scholze <dannyscholze21@gmail.com>
2025-08-23 09:51:30 -0400

    Método buildHangmanPathsPositions criado

--- NTT DATA - Java e IA para Iniciantes/Debugging em Java/Projeto de Jogo da Forca/hangman/src/main/java/br/com/dio/hangman/model/HangmanGame.java
+++ NTT DATA - Java e IA para Iniciantes/Debugging em Java/Projeto de Jogo da Forca/hangman/src/main/java/br/com/dio/hangman/model/HangmanGame.java
@@ -1,5 +1,6 @@
 package br.com.dio.hangman.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static br.com.dio.hangman.model.HangmanGameStatus.PENDING;
@@ -10,6 +11,7 @@ public class HangmanGame {
     private final static int HANGMAN_INITIAL_LINE_LENGTH_WITH_LINE_SEPARATOR = 10;
 
     private final int lineSize;
+    private final List<HangmanChar> hangmanPaths;
     private final List<HangmanChar> characters;
 
     private String hangman;
@@ -27,6 +29,23 @@ public class HangmanGame {
     @Override
     public String toString() {
         return this.hangman;
+    }
+
+    private List<HangmanChar> buildHangmanPathsPositions(){
+        final var HEAD_LINE = 3;
+        final var BODY_LINE = 4;
+        final var LEGS_LINE = 5;
+        return new ArrayList<>(
+               List.of(
+                       new HangmanChar('0', this.lineSize * HEAD_LINE + 6),
+                       new HangmanChar('|', this.lineSize * BODY_LINE + 6),
+                       new HangmanChar('/', this.lineSize * BODY_LINE + 5),
+                       new HangmanChar('\\', this.lineSize * BODY_LINE + 7),
+                       new HangmanChar('/', this.lineSize * LEGS_LINE + 5),
+                       new HangmanChar('\\', this.lineSize * LEGS_LINE + 7)
+
+               )
+        );
     }
 
     private List<HangmanChar> setCharacterSpacesPositionInGame(final List<HangmanChar> characters, final int whiteSpacesAmount){
