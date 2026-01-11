c0df840f0ef7900ccb9c3aca9feca62a0830b656
Danny Scholze <dannyscholze21@gmail.com>
2026-01-11 12:04:01 -0400

    atualizano com a nova versão

--- Accenture - Desenvolvimento Java & Cloud/Desenvolvimento de APIs REST com Java e Spring Boot/Introdução ao desenvolvimento de API/my-web-api/src/main/java/dio/web/api/doc/SwaggerConfig.java
+++ Accenture - Desenvolvimento Java & Cloud/Desenvolvimento de APIs REST com Java e Spring Boot/Introdução ao desenvolvimento de API/my-web-api/src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -1,31 +1,31 @@
 package dio.web.api.doc;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-    private Contact contato(){
-        return new Contact(
-                "Seu nome",
-                "http://www.seusite.com.br",
-                "voce@seusite.com.br"
-        );
-    }
-    private ApiInfoBuilder informacoesApi(){
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
 
-        apiInfoBuilder.title("Title - Rest API");
-        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
-        apiInfoBuilder.version("1.0");
-        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
-        apiInfoBuilder.license("Licença - Sua Empresa");
-        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
-        apiInfoBuilder.contact(this.contato());
-
-        return apiInfoBuilder;
+    @Bean
+    public OpenAPI detalheApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Title - Rest API")
+                        .description("API exemplo de uso de Springboot REST API")
+                        .version("1.0")
+                        .termsOfService("Termo de uso: Open Source")
+                        .contact(new Contact()
+                                .name("Seu nome")
+                                .url("http://www.seusite.com.br")
+                                .email("voce@seusite.com.br"))
+                        .license(new License()
+                                .name("Licença - Sua Empresa")
+                                .url("http://www.seusite.com.br")));
     }
+
 }
