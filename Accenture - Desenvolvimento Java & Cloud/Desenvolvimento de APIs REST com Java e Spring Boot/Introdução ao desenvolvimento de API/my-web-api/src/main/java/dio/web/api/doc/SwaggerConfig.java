c21565c47e626959d24098fdb49d37f89cb9fab6
Danny Scholze <dannyscholze21@gmail.com>
2026-01-11 10:03:15 -0400

    adicionando ApiInfoBuilder

--- Accenture - Desenvolvimento Java & Cloud/Desenvolvimento de APIs REST com Java e Spring Boot/Introdução ao desenvolvimento de API/my-web-api/src/main/java/dio/web/api/doc/SwaggerConfig.java
+++ Accenture - Desenvolvimento Java & Cloud/Desenvolvimento de APIs REST com Java e Spring Boot/Introdução ao desenvolvimento de API/my-web-api/src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package dio.web.api.doc;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.Contact;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,5 +14,18 @@ public class SwaggerConfig {
                 "http://www.seusite.com.br",
                 "voce@seusite.com.br"
         );
+    }
+    private ApiInfoBuilder informacoesApi(){
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa");
+        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
     }
 }
